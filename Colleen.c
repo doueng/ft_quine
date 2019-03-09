cbf66d50814d917e69951c962475810ef2699d9e
douglas engstrand <doueng09@gmail.com>
2019-03-09 12:17:48 +0100

    fixed comments

--- Colleen.c
+++ Colleen.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-// hello
+/*
+  comment
+*/
 void rofl() {}
 int main()
 {
-	// hhaha
+	/*
+	  comment
+	*/
 	rofl();
-	char *str = "#include <stdio.h>%2$c// hello%2$cvoid rofl() {}%2$cint main()%2$c{%2$c%3$c// hhaha%2$c%3$crofl();%2$c%3$cchar *str = %4$c%s%4$c;%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}%2$c";
+	char *str = "#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$cvoid rofl() {}%2$cint main()%2$c{%2$c%3$c/*%2$c%3$c  comment%2$c%3$c*/%2$c%3$crofl();%2$c%3$cchar *str = %4$c%s%4$c;%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}%2$c";
 	printf(str, str, 10, 9, 34);
 }
--- Grace.c
+++ Grace.c
@@ -1,6 +1,8 @@
 #include <fcntl.h>
 #include <stdio.h>
-// comment
+/*
+  comment
+*/
 #define MAIN(str) int main () {int fd=open("Grace_kid.c",O_WRONLY|O_CREAT|O_TRUNC,0755);dprintf(fd, str, str, 10, 34);}
 
-MAIN("#include <fcntl.h>%2$c#include <stdio.h>%2$c// comment%2$c#define MAIN(str) int main () {int fd=open(%3$cGrace_kid.c%3$c,O_WRONLY|O_CREAT|O_TRUNC,0755);dprintf(fd, str, str, 10, 34);}%2$c%2$cMAIN(%3$c%1$s%3$c);%2$c");
+MAIN("#include <fcntl.h>%2$c#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$c#define MAIN(str) int main () {int fd=open(%3$cGrace_kid.c%3$c,O_WRONLY|O_CREAT|O_TRUNC,0755);dprintf(fd, str, str, 10, 34);}%2$c%2$cMAIN(%3$c%1$s%3$c);%2$c");
