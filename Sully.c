e3a98cf00a8a3d7bde6d1b14d82c31ae9400d148
douglas engstrand <doueng09@gmail.com>
2019-03-09 14:47:09 +0100

    sully works

--- Sully.c
+++ Sully.c
@@ -1,43 +1,41 @@
 int i = 5;
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(void)
 {
 	char		*new_filename;
-	char		*joined;
+	char		*curr_filename;
+	char		*compile_cmd;
 	int			curr_fd;
 	struct stat	st;
 	int			new_fd;
 	char		*new_file;
-	char		*curr_file;
 
-	curr_file = strdup(__FILE__);
-	if (i <= 0)
+	if (i-- <= 0)
 		return (0);
-	if (i == 5)
-	{
-		curr_file[5] = 0;
-		asprintf(&joined, "%s_%d.c", curr_file, --i);
-	}
-	else
-	{
-		curr_file[6] = i;
-		joined = curr_file;
-	}
-	curr_fd = open(__FILE__, O_RDONLY);
-	new_file = malloc(st.st_size);
-	new_fd = open(joined, O_CREAT|O_TRUNC, 0755);
-	new_file[8] = i;
-	read(new_fd, new_file, st.st_size);
-	char *compile_cmd;
+	curr_filename = strdup(__FILE__);
+	curr_filename[5] = 0;
+	asprintf(&new_filename, "%s_%d.c", curr_filename, i);
 
-	asprintf(&compile_cmd, "gcc -Werror -Wextra -Wall %s", curr_file);
+	curr_fd = open(__FILE__, O_RDONLY);
+	fstat(curr_fd, &st);
+
+	new_file = malloc(st.st_size);
+	read(curr_fd, new_file, st.st_size);
+
+	new_fd = open(new_filename, O_CREAT|O_TRUNC|O_RDWR, 0755);
+	new_file[8] = i + '0';
+	write(new_fd, new_file, st.st_size);
+
+	asprintf(&compile_cmd, "gcc -Werror -Wextra -Wall %s", new_filename);
 	system(compile_cmd);
 	system("./a.out");
 	return (0);
