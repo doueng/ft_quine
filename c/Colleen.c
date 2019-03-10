05e5a1cd12c813e80e2bde83ea9e0f020e589433
Douglas ENGSTRAND <dengstra@e1r1p7.42.fr>
2019-03-10 20:03:47 +0100

    better

--- c/Colleen.c
+++ c/Colleen.c
@@ -2,13 +2,7 @@
 /*
   comment
 */
-void rofl() {}
-int main()
-{
+void rofl(){}int main(){
 	/*
 	  comment
-	*/
-	rofl();
-	char *str = "#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$cvoid rofl() {}%2$cint main()%2$c{%2$c%3$c/*%2$c%3$c  comment%2$c%3$c*/%2$c%3$crofl();%2$c%3$cchar *str = %4$c%s%4$c;%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}%2$c";
-	printf(str, str, 10, 9, 34);
-}
+	*/rofl();char *str="#include <stdio.h>%2$c#include <string.h>%2$c/*%2$c%3$ccomment%2$c*/%2$cvoid rofl(){} int main(){%2$c/*%2$c%3$ccomment%2$c*/rofl();char *str=strdup(%4$c%s%4$c);printf(str,str,10,9,34);}";printf(str,str,10,9,34);}
--- c/Sully.c
+++ c/Sully.c
@@ -1,32 +1,14 @@
-int i = 5;
-
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-
-int main(void)
-{
-	char		*new_filename;
-	char		*curr_filename;
-	char		*compile_cmd;
-	int			new_fd;
-	char		*new_file;
-	char		*new_exe;
-
-	if (i-- <= 4) return (0);
-	if (NULL == (curr_filename = strndup(__FILE__, 5))) return (1);
-	asprintf(&new_filename, "%s_%d.c", curr_filename, i);
-
-	asprintf(&new_file, "rofl %d alsdjflasjdflkj", i);
-	new_file[8] = i + '0';
-
-	if (-1 == (new_fd = open(new_filename, O_CREAT|O_TRUNC|O_RDWR, 0755))) return (1);
-	dprintf(new_fd, "%s", new_file);
-
-	new_exe = strdup(new_filename);
-	new_exe[5] = 0;
-	asprintf(&compile_cmd, "gcc -Werror -Wextra -Wall %1$s -o %2$s; ./%2$s", new_filename, new_exe);
-	system(compile_cmd);
-}
+int main(void){
+	char *new,*cmd;
+	int fd,i=5;
+	if (i-- <= 4)return(0);
+	asprintf(&new,"Sully_%d.c",i);
+	char *str=strdup("");
+	if(-1==(fd=open(new,O_CREAT|O_TRUNC|O_RDWR,0755)))return(1);
+	dprintf(fd,str,str,10,9,34);
+	asprintf(&cmd,"gcc -Werror -Wextra -Wall %1$s -o Sully;./Sully",new);
+	system(cmd);}
