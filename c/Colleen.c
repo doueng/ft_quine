07cee458f3341c003fe5ef50587987489866cf4f
Douglas ENGSTRAND <dengstra@e1r1p7.42.fr>
2019-03-10 20:36:31 +0100

    colleen works on school computer

--- c/Colleen.c
+++ c/Colleen.c
@@ -2,7 +2,7 @@
 /*
   comment
 */
-void rofl(){}int main(){
+void rofl(){} int main(){
 	/*
 	  comment
-	*/rofl();char *str="#include <stdio.h>%2$c#include <string.h>%2$c/*%2$c%3$ccomment%2$c*/%2$cvoid rofl(){} int main(){%2$c/*%2$c%3$ccomment%2$c*/rofl();char *str=strdup(%4$c%s%4$c);printf(str,str,10,9,34);}";printf(str,str,10,9,34);}
+	*/rofl();char *str="#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$cvoid rofl(){} int main(){%2$c%3$c/*%2$c%3$c  comment%2$c%3$c*/rofl();char *str=%4$c%1$s%4$c;printf(str,str,10,9,34);}%2$c";printf(str,str,10,9,34);}
