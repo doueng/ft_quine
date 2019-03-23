b84e362eb42e6813dabf3c0ede86f502de220604
Douglas ENGSTRAND <dengstra@e1r4p17.42.fr>
2019-03-23 17:31:33 +0100

    better

--- clojurescript/Sully.cljs
+++ clojurescript/Sully.cljs
@@ -4,36 +4,48 @@
 
 (def s 686)
 (def exec (.-exec (js/require 6child_process6)))
-(def curr_filename (.basename (js/require 6path6) js/__filename))
+(def curr_filename
+  (apply str
+         (reverse
+          (take-while #(not= %1 (char (+ 45 2))) (reverse js/__filename)))))
 (def i 7)
 (def n (- i (if (not= curr_filename 6Sully.cljs6) 1 0)))
-(def new_filename (apply str 6Sully_6 n 6.cljs6))
+(def new_filename (str 6Sully_6 n 6.cljs6))
 
-(fs/writeFileSync new_filename
-                  (reduce
-                   #(+ %1
-                       (cond
-                         (= %2 (str (+ 3 3))) (str (char 34))
-                         (= %2 (str (+ 4 4))) s
-                         (= %2 (str (- 9 2))) n
-                         :else %2))
-                       (str)
-                       s))
-    (and (> i 0) (exec (str 6lumo6 new_filename))) ")
+(and (> i 0)
+     (do
+     (fs/writeFileSync new_filename
+                       (reduce
+                        #(+ %1
+                            (cond
+                              (= %2 (str (+ 3 3))) (str (char 34))
+                              (= %2 (str (+ 4 4))) s
+                              (= %2 (str (- 9 2))) n
+                              :else %2))
+                        (str)
+                        s))
+     (exec (str 6lumo 6 new_filename))))
+")
 (def exec (.-exec (js/require "child_process")))
-(def curr_filename (.basename (js/require "path") js/__filename))
+(def curr_filename
+  (apply str
+         (reverse
+          (take-while #(not= %1 (char (+ 45 2))) (reverse js/__filename)))))
 (def i 5)
 (def n (- i (if (not= curr_filename "Sully.cljs") 1 0)))
-(def new_filename (apply str "Sully_" n ".cljs"))
+(def new_filename (str "Sully_" n ".cljs"))
 
-(fs/writeFileSync new_filename
-                  (reduce
-                   #(+ %1
-                       (cond
-                         (= %2 (str (+ 3 3))) (str (char 34))
-                         (= %2 (str (+ 4 4))) s
-                         (= %2 (str (- 9 2))) n
-                         :else %2))
-                       (str)
-                       s))
-    (and (> i 0) (exec (str "lumo" new_filename)))
+(and (> i 0)
+     (do
+       (fs/writeFileSync new_filename
+                         (reduce
+                          #(+ %1
+                              (cond
+                                (= %2 (str (+ 3 3))) (str (char 34))
+                                (= %2 (str (+ 4 4))) s
+                                (= %2 (str (- 9 2))) n
+                                :else %2))
+                          (str)
+                          s))
+       (exec (str "lumo " new_filename))))
+
