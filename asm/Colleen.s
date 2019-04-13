7940310f54c6f8e6b928c339a79e9fb374b8b43f
Douglas ENGSTRAND <dengstra@e2r8p3.42.fr>
2019-04-13 15:42:56 +0200

    added missing comment to colleen

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -5,13 +5,13 @@
 	;; comment
 	section	.text
 _main:
+	;; comment
 	sub		rsp, 8
 	call	_rofl
 	lea		rdi, [rel msg]
 	lea		rsi, [rel msg]
 	mov		rdx, 10
-	mov		rcx, 9
-	mov		r8, 34
+	mov		rcx, 34
 	call	_printf
 	add		rsp, 8
 
@@ -19,4 +19,4 @@ _rofl:
 	ret
 
 	section	.data
-msg:db		"%3$cglobal%3$c_main%2$c%2$c%3$cextern%3$c_printf%2$c%2$c%3$c;; comment%2$c%3$csection%3$c.text%2$c_main:%2$c%3$csub%3$c%3$crsp, 8%2$c%3$ccall%3$c_rofl%2$c%3$clea%3$c%3$crdi, [rel msg]%2$c%3$clea%3$c%3$crsi, [rel msg]%2$c%3$cmov%3$c%3$crdx, 10%2$c%3$cmov%3$c%3$crcx, 9%2$c%3$cmov%3$c%3$cr8, 34%2$c%3$ccall%3$c_printf%2$c%3$cadd%3$c%3$crsp, 8%2$c%2$c_rofl:%2$c%3$cret%2$c%2$c%3$csection%3$c.data%2$cmsg:db%3$c%3$c%4$c%1$s%4$c%2$c"
+msg:db		"	global	_main%2$c%2$c	extern	_printf%2$c%2$c	;; comment%2$c	section	.text%2$c_main:%2$c	;; comment%2$c	sub		rsp, 8%2$c	call	_rofl%2$c	lea		rdi, [rel msg]%2$c	lea		rsi, [rel msg]%2$c	mov		rdx, 10%2$c	mov		rcx, 34%2$c	call	_printf%2$c	add		rsp, 8%2$c%2$c_rofl:%2$c	ret%2$c%2$c	section	.data%2$cmsg:db		%3$c%1$s%3$c%2$c"
