43275714021f8540a2bf3bd7c27195d9abc77427
Douglas ENGSTRAND <dengstra@e2r8p3.42.fr>
2019-04-13 15:34:49 +0200

    cleaner colleen

--- c/Colleen.c
+++ c/Colleen.c
@@ -2,7 +2,12 @@
 /*
   comment
 */
-void rofl(){} int main(){
+void rofl(){}
+int main(){
 	/*
 	  comment
-	*/rofl();char *str="#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$cvoid rofl(){} int main(){%2$c%3$c/*%2$c%3$c  comment%2$c%3$c*/rofl();char *str=%4$c%1$s%4$c;printf(str,str,10,9,34);}%2$c";printf(str,str,10,9,34);}
+	*/
+	rofl();
+	char *str="#include <stdio.h>%2$c/*%2$c  comment%2$c*/%2$cvoid rofl(){}%2$cint main(){%2$c	/*%2$c	  comment%2$c	*/%2$c	rofl();%2$c	char *str=%3$c%1$s%3$c;%2$c	printf(str,str,10,34);%2$c}%2$c";
+	printf(str,str,10,34);
+}
