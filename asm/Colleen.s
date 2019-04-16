7e13aaad1cdc78312f31447886fa61c3b0a3c93a
Douglas ENGSTRAND <dengstra@e2r6p16.42.fr>
2019-04-16 13:29:05 +0200

    fixes

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -14,9 +14,10 @@ _main:
 	mov		rcx, 34
 	call	_printf
 	add		rsp, 8
+	ret
 
 _rofl:
 	ret
 
 	section	.data
-msg:db		"	global	_main%2$c%2$c	extern	_printf%2$c%2$c	;; comment%2$c	section	.text%2$c_main:%2$c	;; comment%2$c	sub		rsp, 8%2$c	call	_rofl%2$c	lea		rdi, [rel msg]%2$c	lea		rsi, [rel msg]%2$c	mov		rdx, 10%2$c	mov		rcx, 34%2$c	call	_printf%2$c	add		rsp, 8%2$c%2$c_rofl:%2$c	ret%2$c%2$c	section	.data%2$cmsg:db		%3$c%1$s%3$c%2$c"
+msg:db		"	global	_main%2$c%2$c	extern	_printf%2$c%2$c	;; comment%2$c	section	.text%2$c_main:%2$c	;; comment%2$c	sub		rsp, 8%2$c	call	_rofl%2$c	lea		rdi, [rel msg]%2$c	lea		rsi, [rel msg]%2$c	mov		rdx, 10%2$c	mov		rcx, 34%2$c	call	_printf%2$c	add		rsp, 8%2$c	ret%2$c%2$c_rofl:%2$c	ret%2$c%2$c	section	.data%2$cmsg:db		%3$c%1$s%3$c%2$c"
--- c/Sully.c
+++ c/Sully.c
@@ -8,9 +8,9 @@ int main(void){
 	int i=5;
 	if (i <= 0)return(0);
 	i--;
-	i+=strcmp(__FILE__,"Sully.c")==0?1:0;
+	i+=strstr(__FILE__,"Sully.c")?1:0;
 	asprintf(&new, "Sully_%d.c", i);
-	char *str="#include <stdio.h>%2$c#include <fcntl.h>%2$c#include <stdlib.h>%2$c#include <string.h>%2$cint main(void){%2$c	char*new=NULL,*cmd=NULL;%2$c	int fd;%2$c	int i=%4$d;%2$c	if (i <= 0)return(0);%2$c	i--;%2$c	i+=strcmp(__FILE__,%3$cSully.c%3$c)==0?1:0;%2$c	asprintf(&new, %3$cSully_%%d.c%3$c, i);%2$c	char *str=%3$c%1$s%3$c;%2$c	if(-1==(fd=open(new,O_CREAT|O_TRUNC|O_RDWR,0640)))return(1);%2$c	dprintf(fd,str,str,10,34,i);%2$c	asprintf(&cmd,%3$cgcc -Werror -Wextra -Wall %%s -o Sully_%%d;./Sully_%%d%3$c,new,i,i);%2$c	system(cmd);%2$c}%2$c";
+	char *str="#include <stdio.h>%2$c#include <fcntl.h>%2$c#include <stdlib.h>%2$c#include <string.h>%2$cint main(void){%2$c	char*new=NULL,*cmd=NULL;%2$c	int fd;%2$c	int i=%4$d;%2$c	if (i <= 0)return(0);%2$c	i--;%2$c	i+=strstr(__FILE__,%3$cSully.c%3$c)?1:0;%2$c	asprintf(&new, %3$cSully_%%d.c%3$c, i);%2$c	char *str=%3$c%1$s%3$c;%2$c	if(-1==(fd=open(new,O_CREAT|O_TRUNC|O_RDWR,0640)))return(1);%2$c	dprintf(fd,str,str,10,34,i);%2$c	asprintf(&cmd,%3$cgcc -Werror -Wextra -Wall %%s -o Sully_%%d;./Sully_%%d%3$c,new,i,i);%2$c	system(cmd);%2$c}%2$c";
 	if(-1==(fd=open(new,O_CREAT|O_TRUNC|O_RDWR,0640)))return(1);
 	dprintf(fd,str,str,10,34,i);
 	asprintf(&cmd,"gcc -Werror -Wextra -Wall %s -o Sully_%d;./Sully_%d",new,i,i);
